9308b31ecce1fab155fdcc4c0b91c87bb506f310
Gaburiere <gabriele.coppola@giuneco.it>
2020-11-08 19:15:54 +0100

    manage droid push notification

---
+++ notifications.mobile.core/Classes/AppConstants.cs
@@ -0,0 +1,22 @@
+namespace notification.mobile.core.Classes
+{
+    public static class AppConstants
+    {
+        public static string NotificationChannelName { get; set; } = "XamarinNotifyChannel";
+        
+        /// <summary>
+        /// the name of the Azure Notification Hub you created in your Azure portal.
+        /// </summary>
+        public static string NotificationHubName { get; set; } = "formazionenhub";
+        
+        /// <summary>
+        /// This value is found in the Azure Notification Hub under Access Policies.
+        /// </summary>
+        public static string ListenConnectionString { get; set; } = 
+            "Endpoint=sb://formazionenhubns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=0M+ggpBddNkRSNr9e4S57k/ImlaiJsi80diYSpylWgE=";
+        public static string DebugTag { get; set; } = "XamarinNotify";
+        public static string[] SubscriptionTags { get; set; } = { "default" };
+        public static string FCMTemplateBody { get; set; } = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+        public static string APNTemplateBody { get; set; } = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
+    }
+}
